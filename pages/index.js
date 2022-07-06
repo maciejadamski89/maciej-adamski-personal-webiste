b14b381eb49b1099fca97d962c245f5a6969646f
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-06 23:26:53 +0200

    Add skills section

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar/";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
+import SKills from "@/components/Skills";
 import { HeadContent } from "@/content/Head";
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
         <Hero />
         <About />
         <Experience />
+        <SKills />
       </main>
     </>
   );
