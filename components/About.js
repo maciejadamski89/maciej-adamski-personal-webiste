493173dfcc14f85b43805df6536550f1f7961204
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-06 23:08:07 +0200

    Change about me text

--- components/About.js
+++ components/About.js
@@ -3,22 +3,21 @@ import { DotBig, DotSmall } from "./Dot";
 import Underline from "./Underline";
 
 export default function About() {
+  const content = `
+      Hi, my name is Maciej. I'm currently working as a Software Developer and developing a large project for Michelin. In my free time I move to the world of music with the sound of the piano. I am a happy dad as well as a dogmaster for my two beloved dogs. 
+  `;
+
   return (
     <>
       <section
         id="about"
-        className="max-w-4xl mx-auto space-y-8 text-center mt-32"
+        className="max-w-4xl mx-auto mt-32 space-y-8 text-center"
       >
         <h2 className="text-white">
           About me
           <DotSmall />
         </h2>
-        <p className="2xl:text-2xl text-gray-400">
-          Hi, I'm Maciej. I deal with software development, package management,
-          automation and web design. I can easily establish new contacts. I'm
-          strongly involved in every project. Performance and design are key for
-          me.
-        </p>
+        <p className="text-gray-400 2xl:text-2xl">{content}</p>
         <div>
           <Button title="Get in touch" href="#contact" />
         </div>
