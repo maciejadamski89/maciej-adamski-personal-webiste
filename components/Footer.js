f3363abb67506c0f5231be91d885f52ac62ec40a
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-07 14:23:21 +0200

    Add footer

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <>
+      <footer
+        id="footer"
+        className="py-10 mt-32 text-xl text-center text-gray-400"
+      >
+        <p>© Copyright 2022. Created by Maciej Adamski.</p>
+      </footer>
+    </>
+  );
+}
