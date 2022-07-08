e6af6dc24ab68752a4320fbd900c6b6dc0a09a9b
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-08 17:45:28 +0200

    Add grid for xl

--- components/Projects.js
+++ components/Projects.js
@@ -16,64 +16,66 @@ export default function About() {
           Projects
           <DotSmall />
         </h2>
-        <div>
-          <div className="max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">My resume page</div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                Which is actually this page. I'm currently still working on it.
-                I want to make it in the right way which is not the same as
-                simplest.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">DevOps Portal</div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                I am building a platform/portal for our major French client -
-                Michelin. The portal is designed to facilitate the management of
-                production applications and servers. The project originated from
-                my idea and initiative.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">
-                <Link href="https://github.com/zubin-madon/bogoshipo_website">
-                  <a className="underline underline-offset-2 hover:text-blue-800">
-                    Bogoshipo
-                  </a>
-                </Link>{" "}
-                website
+        <div className="gap-8 xl:grid xl:grid-cols-2">
+          <div>
+            <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+              <div className="px-6 py-4">
+                <div className="mb-2 text-xl font-bold">My resume page</div>
+                <p className="mt-4 text-xl text-gray-400 ">
+                  Which is actually this page. I'm currently still working on
+                  it. I want to make it in the right way which is not the same
+                  as simplest.
+                </p>
               </div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                Bogoshipo is a Blockchain Pet Game in progress. This website
-                provides information on the development and architecture.
-              </p>
             </div>
           </div>
-        </div>
-        <div>
-          <div className="max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">
-                <Link href="https://github.com/zubin-madon/bogoshipo_website">
-                  <a className="underline underline-offset-2 hover:text-blue-800">
-                    Template-bhai
-                  </a>
-                </Link>{" "}
+          <div>
+            <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+              <div className="px-6 py-4">
+                <div className="mb-2 text-xl font-bold">DevOps Portal</div>
+                <p className="mt-4 text-xl text-gray-400 ">
+                  I am building a platform/portal for our major French client -
+                  Michelin. The portal is designed to facilitate the management
+                  of production applications and servers. The project originated
+                  from my idea and initiative.
+                </p>
               </div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                A growing project that is being developed with the help of the
-                react community. The project aims to provide ready-made
-                templates for TailwindCSS.
-              </p>
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+              <div className="px-6 py-4">
+                <div className="mb-2 text-xl font-bold">
+                  <Link href="https://github.com/zubin-madon/bogoshipo_website">
+                    <a className="underline underline-offset-2 hover:text-blue-800">
+                      Bogoshipo
+                    </a>
+                  </Link>{" "}
+                  website
+                </div>
+                <p className="mt-4 text-xl text-gray-400 ">
+                  Bogoshipo is a Blockchain Pet Game in progress. This website
+                  provides information on the development and architecture.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+              <div className="px-6 py-4">
+                <div className="mb-2 text-xl font-bold">
+                  <Link href="https://github.com/zubin-madon/bogoshipo_website">
+                    <a className="underline underline-offset-2 hover:text-blue-800">
+                      Template-bhai
+                    </a>
+                  </Link>{" "}
+                </div>
+                <p className="mt-4 text-xl text-gray-400 ">
+                  A growing project that is being developed with the help of the
+                  react community. The project aims to provide ready-made
+                  templates for TailwindCSS.
+                </p>
+              </div>
             </div>
           </div>
         </div>
