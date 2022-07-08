5265d0126a489d77f30fd793ba4a51e5466421a7
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-08 13:45:30 +0200

    Change navlink depends if its home

--- components/Navbar/Navlink.js
+++ components/Navbar/Navlink.js
@@ -1,11 +1,21 @@
-export default function Navlink({ name, url }) {
+export default function Navlink({ name, url, home }) {
   return (
     <>
-      <li className="text-gray-100 hover:text-gray-300 text-lg">
-        <a href={url} className="hover:py-2 hover:border-b-4 border-blue-800 ">
-          {name}
-        </a>
-      </li>
+      {home && (
+        <li className="px-4 py-2 text-lg text-gray-100 bg-blue-800 rounded-2xl hover:text-gray-300">
+          <a href={url}>{name}</a>
+        </li>
+      )}
+      {!home && (
+        <li className="text-lg text-gray-100 hover:text-gray-300">
+          <a
+            href={url}
+            className="border-blue-800 hover:py-2 hover:border-b-4 "
+          >
+            {name}
+          </a>
+        </li>
+      )}
     </>
   );
 }
