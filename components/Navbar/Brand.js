89d47f1060196093dd92434c13c0dde6e1cc78ad
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-11 10:07:30 +0200

    Fix navbar responsivnes

--- components/Navbar/Brand.js
+++ components/Navbar/Brand.js
@@ -4,7 +4,7 @@ import { DotSmall } from "../Dot";
 export default function Brand() {
   return (
     <>
-      <div className="text-lg font-bold text-gray-100 hover:text-gray-300">
+      <div className="text-xl font-bold text-gray-100 hover:text-gray-300">
         <Link href="/">
           <a>
             Maciej Adamski
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,47 +1,18 @@
 import Brand from "./Brand";
 import Items from "./Items";
+import Button from "@/components/Button";
 
 export default function Navbar() {
   return (
     <>
       <header>
-        <nav className="md:fixed top-0 left-0 right-0 z-10 flex items-center justify-center md:justify-between w-full lg:px-40 py-8 mx-auto bg-[#181818]">
+        <nav className="md:fixed top-0 left-0 right-0 z-10 flex items-center justify-between xl:max-w-7xl w-full p-4 sm:p-8 mx-auto bg-[#181818]">
           <Brand />
           <Items />
-        </nav>
-        <hr className="mb-8 border-neutral-800"></hr>
-
-        {/* Mobile Navbar */}
-        {/* <nav className="z-50 block md:hidden">
-          <div className="fixed bottom-0 left-0 right-0 bg-neutral-900">
-            <div className="grid grid-cols-4 h-[80px] gap-1 px-1 ">
-              <a
-                href="#experience"
-                className="flex items-center justify-center text-sm bg-blue-900 rounded-lg"
-              >
-                <p>Experience</p>
-              </a>
-              <a
-                href="#skills"
-                className="flex items-center justify-center text-sm bg-blue-900 rounded-lg"
-              >
-                <p>Skills</p>
-              </a>
-              <a
-                href="#projects"
-                className="flex items-center justify-center text-sm bg-blue-900 rounded-lg"
-              >
-                <p>Projects</p>
-              </a>
-              <a
-                href="#contact"
-                className="flex items-center justify-center text-sm bg-blue-900 rounded-lg"
-              >
-                <p>Contact</p>
-              </a>
-            </div>
+          <div className="lg:hidden">
+            <Button title="Contact" href="#contact" />
           </div>
-        </nav> */}
+        </nav>
       </header>
     </>
   );
