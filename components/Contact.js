6174cf63a698e13e6a45c7303f3dc529a374351b
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-13 15:17:57 +0200

    Add icones and more space

--- components/Contact.js
+++ components/Contact.js
@@ -15,11 +15,43 @@ export default function Contact() {
           Contact
           <DotSmall />
         </h2>
-        <p className="text-lg text-gray-400 2xl:text-2xl">+48 510 180 212</p>
-        <p className="text-lg text-gray-400 2xl:text-2xl">
-          maciej.adamski89@gmail.com
-        </p>
-        <div className="grid grid-cols-4 gap-0 mx-auto">
+        <div className="flex items-center justify-center space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
+            />
+          </svg>
+          <p className="text-lg text-gray-400 2xl:text-2xl">+48 510 180 212</p>
+        </div>
+        <div className="flex items-center justify-center space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg>
+          <p className="text-lg text-gray-400 2xl:text-2xl">
+            maciej.adamski89@gmail.com
+          </p>
+        </div>
+        <div className="grid grid-cols-4 gap-0 mx-auto !mt-16">
           <div className="flex justify-center">
             <TwitterIcon
               width="50"
