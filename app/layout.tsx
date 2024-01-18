70a0cae72bb4b19c3d01710f01d0de9e1772bd66
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-01-18 12:05:01 +0100

    ref: change font to Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,15 @@
-"use client";
 import "./globals.css";
-
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import TransitionEffect from "@/components/TransitionEffect";
+import {GeistSans} from "geist/font/sans";
+import {GeistMono} from "geist/font/mono";
 
-import {Inter} from "next/font/google";
-
-const inter = Inter({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-inter",
-});
-
-function GradientBlurBackground() {
-	return (
-		<div className="absolute top-0 left-0 w-full rounded-md opacity-50 h-96 bg-gradient-to-br from-indigo-800 to-indigo-950 filter blur-3xl -z-50"></div>
-	);
-}
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className} antialiased bg-dark h-full`}>
-				<TransitionEffect />
+		<html lang="en" className="h-full">
+			<body className={`${GeistMono.className} antialiased bg-dark min-h-full`}>
 				<Navbar />
-				<GradientBlurBackground />
-				<div>{children}</div>
+				<main>{children}</main>
 				<Footer />
 			</body>
 		</html>
